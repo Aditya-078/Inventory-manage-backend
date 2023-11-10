2df49579ffa8d64162a25bba398ce58673c2e807
Aditya-078 <200303105146@paruluniversity.ac.in>
2023-11-10 22:00:52 +0530

    changes

--- server.js
+++ server.js
@@ -44,7 +44,8 @@ app.use(
   cors({
     origin: ["http://localhost:3000", "https://infispecinventorymanage.vercel.app"],
     credentials: true,
-    methods:["GET", "POST"," PUT", "DELETE"]
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "optionsSuccessStatus": 204
   })
 );
 
